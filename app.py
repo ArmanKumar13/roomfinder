859fa78edcd519a0cc4e1182061832fa6e51a313
ArmanKumar02 <armankushwaha02@gmail.com>
2026-07-27 19:52:26 +0530

    Update TiDB configuration for Render deployment

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ app.secret_key = config.SECRET_KEY
 # MySQL Configuration
 # -----------------------------
 app.config["MYSQL_HOST"] = config.MYSQL_HOST
+app.config["MYSQL_PORT"] = int(config.MYSQL_PORT)
 app.config["MYSQL_USER"] = config.MYSQL_USER
 app.config["MYSQL_PASSWORD"] = config.MYSQL_PASSWORD
 app.config["MYSQL_DB"] = config.MYSQL_DB
